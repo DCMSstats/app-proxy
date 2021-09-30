8e52b8ae70d489e269db411b6a0c74adc666b0ec
nialldcms <niall.goulding1@dcms.gov.uk>
2021-09-30 11:30:17 +0100

    server

--- main.py
+++ main.py
@@ -13,5 +13,7 @@ app.layout = html.Div([
 )])
 
 
-#if __name__ == '__main__':
-#    app.run_server(debug=True)
+if __name__ == '__main__':
+    #app.run_server(debug=True)
+
+    app.run_server(host='0.0.0.0', port=8080, use_reloader=True)
